d30af4d2e1f201b8e55a1c65e911be4f21a923b1
student <student@ip-172-31-53-54.us-west-2.compute.internal>
2021-08-16 18:27:21 -0700

    Greeter

--- src/_03_print_and_popups/_1_greeter/Greeter.java
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -5,8 +5,8 @@ import javax.swing.JOptionPane;
 public class Greeter {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		String input = JOptionPane.showInputDialog("What is your name?");
-		JOptionPane.showMessageDialog(null,"Hello "+input);
+		String input = JOptionPane.showInputDialog("What is your token?");
+		JOptionPane.showMessageDialog(null,"Here is your token "+input);
 	}
 	
 }

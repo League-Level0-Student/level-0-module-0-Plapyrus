0fcc2089fc713535c47809009a3568c9ee4e22bd
student <student@ip-172-31-52-240.us-west-2.compute.internal>
2021-10-18 18:27:06 -0700

    template

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -10,17 +10,27 @@ public class QuizGame {
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-		String input = JOptionPane.showInputDialog("Do you know what 12 times 37 is?");
+		String one = JOptionPane.showInputDialog("Do you know what 12 times 37 is?");
 				// 3.  Use an if statement to check if their answer is correct
-				if (input.equals("444")) {
+				if (one.equals("444")) {
 					score +=1;
 				}
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
-		
+				String two = JOptionPane.showInputDialog("How many letters are in this sentence.");
+				if (two.equals("31")) {
+					score +=1;
+				}
+				String three = JOptionPane.showInputDialog("What does π equal");
+				if (three.equals("3.14")) {
+					score +=1;
+				}
+				String four = JOptionPane.showInputDialog("How many letters are in this sentence.");
+				if (four.equals("31")) {
+					score +=1;
+				}
 		// After all the questions have been asked, tell the user their final score 
 		
 	}
